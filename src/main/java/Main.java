fb3d8340624a5db71c4aade0f5409c13ed576ebc
Lian Canio <138495779+ChiaxNiro@users.noreply.github.com>
2023-10-15 19:40:34 -0300

    Add files via upload
    segundo commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
     }
     public static Vehiculo seleccionDeAuto(int seleccion){
         switch (seleccion) {
@@ -20,5 +21,23 @@ public class Main {
     public static Persona crearPersona(String nombre){
         Persona persona = new Persona(nombre);
         return persona;
+    }
+    public static Ubicacion seleccionarUbicacion(int seleccion){
+        switch (seleccion) {
+            case 1:
+                Ubicacion pucon = new Ubicacion(105);
+                return pucon;
+            case 2:
+                Ubicacion concepcion = new Ubicacion(292);
+                return concepcion;
+            case 3:
+                Ubicacion victoria = new Ubicacion(64);
+                return victoria;
+        }
+        return null;
+    }
+    public static int calculoTiempo(int velocidad, int distancia){
+        int tiempo = distancia/velocidad;
+        return tiempo;
     }
 }
